6202b615e4407fe5aaf5d5d6111fe7e057a0bcfe
jakobowsky <jakob.wolitzki@gmail.com>
2020-04-19 02:32:25 +0200

    Getting product info works

--- simple_tracker.py
+++ simple_tracker.py
@@ -7,6 +7,8 @@ from web_driver_conf import get_chrome_web_driver
 from web_driver_conf import set_ignore_certificate_error
 from web_driver_conf import set_browser_as_incognito
 from web_driver_conf import set_automation_as_head_less
+from utils import convert_price_toNumber
+from selenium.common.exceptions import NoSuchElementException
 
 
 class AmazonAPI:
@@ -14,10 +16,11 @@ class AmazonAPI:
         self.base_url = "http://www.amazon.de/"
         self.search_term = search_term
         options = get_web_driver_options()
-        # set_automation_as_head_less(options)
+        set_automation_as_head_less(options)
         set_ignore_certificate_error(options)
         set_browser_as_incognito(options)
         self.driver = get_chrome_web_driver(options)
+        self.currency = '€'
         price = "&rh=p_36%3A20000-50000"
 
     def run(self):
@@ -57,29 +60,64 @@ class AmazonAPI:
     def get_single_product_info(self, product_link):
         self.driver.get(product_link)
         asin = self.get_asin(product_link)
-        print("ASIN: ", asin)
         product_short_url = self.shorten_url(asin)
-        print("Short URL: ", product_short_url)
-        print("debug")
-        title = self.driver.find_element_by_id('productTitle').text
-        by = self.driver.find_element_by_id('bylineInfo').text
-        price = self.driver.find_element_by_id('priceblock_ourprice').text
-        # if not price:
-        # self.driver.find_element_by_id('availability').text <- check if ava
-        # if ava:
-        # self.driver.find_element_by_class_name('olp-padding-right').text
+        title = self.get_title()
+        seller = self.get_seller()
+        price = self.get_price()
+        if title and seller and price:
+            product_info = {
+                'asin': asin,
+                'url': product_short_url,
+                'title': title,
+                'seller': seller,
+                'price': price
+            }
+            return product_info
+        return None
 
-        print("Testing this page now")
+    def get_title(self):
+        try:
+            return self.driver.find_element_by_id('productTitle').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get title of a product - {self.driver.current_url}")
+            return None
+
+    def get_seller(self):
+        try:
+            return self.driver.find_element_by_id('bylineInfo').text
+        except Exception as e:
+            print(e)
+            print(f"Can't get seller of a product - {self.driver.current_url}")
+            return None
+
+    def get_price(self):
+        price = None
+        try:
+            price = self.driver.find_element_by_id('priceblock_ourprice').text
+            price = convert_price_toNumber(price, self.currency)
+        except NoSuchElementException:
+            availability = self.driver.find_element_by_id('availability').text
+            if 'Available' in availability:
+                price = self.driver.find_element_by_class_name('olp-padding-right').text
+                price = price[price.find(self.currency):]
+                price = convert_price_toNumber(price, self.currency)
+        except Exception as e:
+            print(e)
+            print(f"Can't get price of a product - {self.driver.current_url}")
+            return None
+        return price
 
     @staticmethod
     def get_asin(product_link):
         return product_link[product_link.find('/dp/') + 4:product_link.find('/ref')]
 
     def shorten_url(self, asin):
-        return self.base_url + '/dp/' + asin
+        return self.base_url + 'dp/' + asin
 
 
 if __name__ == '__main__':
     am = AmazonAPI('PS4')
-    am.get_single_product_info(
-        'https://www.amazon.de/-/en/dp/B07WHSY2WT/ref=sr_1_2?dchild=1&keywords=ps4&qid=1587251983&sr=8-2')
+    info = am.get_single_product_info(
+        'https://www.amazon.de/-/en/9408574/dp/B07KMV94JF/ref=sr_1_5?dchild=1&keywords=ps4&qid=1587255153&sr=8-5')
+    print(info)
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
-def convert_price_toNumber(price):
-    price = price.split("$")[1]
+def convert_price_toNumber(price, currency):
+    price = price.split(currency)[1]
     try:
         price = price.split("\n")[0] + "." + price.split("\n")[1]
     except:
